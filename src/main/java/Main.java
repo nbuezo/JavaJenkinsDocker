1778a5c21c9643f61668fed02c32709734a8f688
nbuezo <noel.buezo@gmail.com>
2021-11-30 18:37:07 -0600

    Despues de credenciales

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println(checkIfInputIsAnEvenNumber(123));//testing
+    }
+    public static boolean checkIfInputIsAnEvenNumber(int number){
+        return  number % 2 == 0;
+    }
+}
